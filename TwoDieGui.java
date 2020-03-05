2b225e62c7554682914a2873ac0b2c87471a3427
Miles C. Stover <55222005+Milzybyte@users.noreply.github.com>
2020-03-05 09:05:18 -0500

    Add files via upload

--- TwoDieGui.java
+++ TwoDieGui.java
@@ -4,6 +4,10 @@ import java.awt.event.*;
 
 public class TwoDieGui extends JFrame implements ActionListener
 {
+	JFrame frame = new JFrame("Roll");
+	Font bigFont = new Font("Arial", Font.BOLD, 38);
+	Font smallFont = new Font("Arial", Font.BOLD,16);
+	Font winning = new Font("Arial", Font.ITALIC,16);
 	JLabel title = new JLabel("Lets roll");
 	JLabel frist = new JLabel("");
 	JTextField roll1 = new JTextField(15); 
@@ -15,11 +19,20 @@ public class TwoDieGui extends JFrame implements ActionListener
 	public TwoDieGui()
 	{
 		super("Two Die");
-		setBounds(700, 450, 220, 180);
+		setBounds(700, 450, 240, 260);
 		setResizable(false);
+		roll1.setEditable(false);
+		roll2.setEditable(false);
+		winner.setEditable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new FlowLayout());
-		
+		button.setBackground(Color.green);
+		getContentPane().setBackground(Color.RED);
+		title.setFont(bigFont);
+		roll1.setFont(smallFont);
+		roll2.setFont(smallFont);
+		winner.setFont(winning);
+		//add(frame);
 		add(title);
 		add(frist);
 		add(roll1);
@@ -66,6 +79,7 @@ public class TwoDieGui extends JFrame implements ActionListener
 	{
 		TwoDieGui rolls = new TwoDieGui();
 		rolls.setVisible(true);
+	
 	}
 	
 	
